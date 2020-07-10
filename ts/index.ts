ec9902fbfef26ebec415ce921c9a3dfc2850211f
Devin Bidwell <dbidwell@intuituscyber.com>
2020-07-10 00:23:41 -0700

    finished-p-and-h-tags

--- ts/index.ts
+++ ts/index.ts
@@ -1,66 +1,107 @@
-interface iHeadingContent {
+interface iTextContent {
   text: string;
   classList?: Array<string>;
   id?: string;
 }
 
-class HeadingElement {
-  protected htmlElement: HTMLHeadingElement;
-  constructor(content: iHeadingContent, createdElement: HTMLHeadingElement) {
-    this.htmlElement = createdElement;
-    if (content.classList.length > 0) {
-      content.classList.forEach((className: string) => {
+interface iClassContent {
+  className?: string;
+  classList?: Array<string>;
+}
+
+abstract class TextContent {
+  protected htmlElement:
+    | HTMLParagraphElement
+    | HTMLHeadingElement
+    | HTMLElement;
+  constructor(
+    content: iTextContent,
+    instanceItem: HTMLElement | HTMLHeadingElement | HTMLParagraphElement
+  ) {
+    this.htmlElement = instanceItem;
+    content.classList.forEach((className: string) => {
+      this.htmlElement.classList.add(className);
+    });
+  }
+  getElement(): HTMLElement | HTMLParagraphElement | HTMLHeadingElement {
+    return this.htmlElement;
+  }
+  removeElement(): void {
+    this.htmlElement.remove();
+  }
+  addClass(classContent: iClassContent): void {
+    if (classContent.classList.length > 0) {
+      classContent.classList.forEach((className: string) => {
         this.htmlElement.classList.add(className);
       });
     }
-    this.htmlElement.id = content.id;
+    if (classContent.className != undefined) {
+      this.htmlElement.classList.add(classContent.className);
+    }
   }
+}
+
+class HeadingElement extends TextContent {
+  protected htmlElement: HTMLHeadingElement;
+
+  constructor(content: iTextContent, instanceItem: HTMLHeadingElement) {
+    super(content, instanceItem);
+  }
+
   getElement(): HTMLHeadingElement {
     return this.htmlElement;
-  }
-  deleteElement(): void {
-    this.htmlElement.remove();
   }
 }
 
 export class H1 extends HeadingElement {
-  constructor(content: iHeadingContent) {
+  constructor(content: iTextContent) {
     const newElement: HTMLHeadingElement = document.createElement("h1");
     super(content, newElement);
   }
 }
 
 export class H2 extends HeadingElement {
-  constructor(content: iHeadingContent) {
+  constructor(content: iTextContent) {
     const newElement: HTMLHeadingElement = document.createElement("h2");
     super(content, newElement);
   }
 }
 
 export class H3 extends HeadingElement {
-  constructor(content: iHeadingContent) {
+  constructor(content: iTextContent) {
     const newElement: HTMLHeadingElement = document.createElement("h3");
     super(content, newElement);
   }
 }
 
 export class H4 extends HeadingElement {
-  constructor(content: iHeadingContent) {
+  constructor(content: iTextContent) {
     const newElement: HTMLHeadingElement = document.createElement("h4");
     super(content, newElement);
   }
 }
 
 export class H5 extends HeadingElement {
-  constructor(content: iHeadingContent) {
+  constructor(content: iTextContent) {
     const newElement: HTMLHeadingElement = document.createElement("h5");
     super(content, newElement);
   }
 }
 
 export class H6 extends HeadingElement {
-  constructor(content: iHeadingContent) {
+  constructor(content: iTextContent) {
     const newElement: HTMLHeadingElement = document.createElement("h6");
     super(content, newElement);
+  }
+}
+
+export class P extends TextContent {
+  htmlElement: HTMLParagraphElement;
+  constructor(content: iTextContent) {
+    const newElement: HTMLParagraphElement = document.createElement("p");
+    super(content, newElement);
+  }
+  getElement(): HTMLParagraphElement {
+    return this.htmlElement;
   }
 }
